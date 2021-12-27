ea7bd8efe9b0998ffa04f800654c2adb901331ee
xlplayer <765026527@qq.com>
2021-12-27 07:57:08 +0000

    change to dgl

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import random
 import pickle
 from tqdm import tqdm
 from model import SessionGraph
-from utils import handle_adj, Data
+from utils import Data
 import torch.nn.functional as F
 import networkx as nx
 import scipy.sparse as sp
@@ -37,84 +37,26 @@ def trans_to_cpu(variable):
         return variable
 
 
-# def seq2graph(seq):
-#     nodes = np.unique(seq)
-#     node2id = {n:i for i,n in enumerate(nodes)}
-#     g = dgl.DGLGraph()
-#     g.add_nodes(len(nodes))
-#     g.ndata['iid'] = torch.tensor(nodes)
-
-#     seq_nid = [node2id[iid] for iid in seq]
-#     g.add_edges(seq_nid, seq_nid)
-#     src = seq_nid[:-1]
-#     dst = seq_nid[1:]
-#     g.add_edges(src, dst)
-#     g.add_edges(dst, src)
-
-#     alias_inputs = [node2id[item] for item in seq]
-#     return [g, alias_inputs]
-
-def seq2graph(seq):
-    items = np.unique(seq)
-    item2id = {n:i for i,n in enumerate(items)}
-    graph_data = {
-        ('item', 'interacts', 'item'):([],[]),
-        ('item', 'agg', 'target'):([],[])
-    }
-    g = dgl.heterograph(graph_data)
-    g = dgl.add_nodes(g, len(items), ntype='item')
-    g.nodes['item'].data['iid'] = torch.tensor(items)
-
-    seq_nid = [item2id[item] for item in seq if item!= 0]
-    g.add_edges(seq_nid, seq_nid, etype='interacts')
-    src = seq_nid[:-1]
-    dst = seq_nid[1:]
-    g.add_edges(src, dst, etype='interacts')
-    g.add_edges(dst, src, etype='interacts')
-    
-    g = dgl.add_nodes(g, 1, ntype='target')
-    g.nodes['target'].data['tid'] = torch.tensor([0])
-    g.add_edges(seq_nid, [0]*len(seq_nid), etype='agg')
-    g.edges['agg'].data['pid'] = torch.tensor(range(len(seq_nid)))
-
-    alias_inputs = [item2id[item] for item in seq]
-    return [g, alias_inputs]
-
 def collate_fn(samples):
-    mask, u_input, target = zip(*samples)
-    adj_n, alias_inputs = zip(*list(map(seq2graph, u_input)))
+    g, target = zip(*samples)
+    g = dgl.batch(g)
+    return g, torch.tensor(target)
 
-    alias_inputs = np.array(alias_inputs)
-    num_nodes = 0
-    for i in range(1, len(adj_n)):
-        num_nodes += len(adj_n[i-1].nodes('item'))
-        alias_inputs[i] = alias_inputs[i] + num_nodes
-
-    adj_n = dgl.batch(adj_n)
-    return torch.tensor(alias_inputs), \
-            torch.tensor(mask), \
-            adj_n, \
-            torch.tensor(target)
-
-     
 def train_test(model, train_data, test_data, epoch):
     print('start training: ', datetime.datetime.now())
     model.train()
 
     total_loss = 0.0
-    train_loader = torch.utils.data.DataLoader(train_data, num_workers=4, batch_size=config.batch_size,
+    train_loader = torch.utils.data.DataLoader(train_data, num_workers=20, batch_size=config.batch_size,
                                                shuffle=True, pin_memory=True, collate_fn=collate_fn)
     
     with tqdm(train_loader) as t:
         for data in t:
             model.optimizer.zero_grad()
-            alias_inputs, mask, adj_n, target = data
-            alias_inputs = trans_to_cuda(alias_inputs).long()
-            mask = trans_to_cuda(mask).float()
-            adj_n = adj_n.to(torch.device('cuda'))
-            # adj = trans_to_cuda(adj)
+            g, target = data
+            g = g.to(torch.device('cuda'))
             targets = trans_to_cuda(target).long()
-            scores =  model(alias_inputs, mask, adj_n)
+            scores =  model(g)
 
             loss = model.loss_function(scores, targets - 1)
             t.set_postfix(loss = loss.item(), lr = model.optimizer.state_dict()['param_groups'][0]['lr'])
@@ -127,24 +69,22 @@ def train_test(model, train_data, test_data, epoch):
 
     print('start predicting: ', datetime.datetime.now())
     model.eval()
-    test_loader = torch.utils.data.DataLoader(test_data, num_workers=4, batch_size=config.batch_size,
+    test_loader = torch.utils.data.DataLoader(test_data, num_workers=20, batch_size=config.batch_size,
                                               shuffle=False, pin_memory=True, collate_fn=collate_fn)
     result = []
     hit20, mrr20 = [], []
     hit10, mrr10 = [], []
     hit5, mrr5 = [], []
     for data in test_loader:
-        alias_inputs, mask, adj_n, target = data
-        alias_inputs = trans_to_cuda(alias_inputs).long()
-        mask = trans_to_cuda(mask).float()
-        adj_n = adj_n.to(torch.device('cuda'))
+        g, target = data
+        g = g.to(torch.device('cuda'))
         targets = trans_to_cuda(target).long()
-        scores =  model(alias_inputs, mask, adj_n)
+        scores =  model(g)
 
         sub_scores = scores.topk(20)[1]
         sub_scores = trans_to_cpu(sub_scores).detach().numpy()
         targets = target.numpy()
-        for score, target, mask in zip(sub_scores, targets, test_data.mask):
+        for score, target in zip(sub_scores, targets):
             hit20.append(np.isin(target - 1, score))
             if len(np.where(score == target - 1)[0]) == 0:
                 mrr20.append(0)
@@ -153,7 +93,7 @@ def train_test(model, train_data, test_data, epoch):
 
         sub_scores = scores.topk(10)[1]
         sub_scores = trans_to_cpu(sub_scores).detach().numpy()
-        for score, target, mask in zip(sub_scores, targets, test_data.mask):
+        for score, target in zip(sub_scores, targets):
             hit10.append(np.isin(target - 1, score))
             if len(np.where(score == target - 1)[0]) == 0:
                 mrr10.append(0)
@@ -162,7 +102,7 @@ def train_test(model, train_data, test_data, epoch):
         
         sub_scores = scores.topk(5)[1]
         sub_scores = trans_to_cpu(sub_scores).detach().numpy()
-        for score, target, mask in zip(sub_scores, targets, test_data.mask):
+        for score, target in zip(sub_scores, targets):
             hit5.append(np.isin(target - 1, score))
             if len(np.where(score == target - 1)[0]) == 0:
                 mrr5.append(0)
--- src/model.py
+++ src/model.py
@@ -79,38 +79,15 @@ class DglAggregator(nn.Module):
 
         self.p = nn.Linear(self.dim, 1, bias=False)
         self.q = nn.Linear(2*self.dim, self.dim, bias=False)
-        self.glu1 = nn.Linear(config.dim, config.dim)
+        self.r = nn.Linear(2*self.dim, self.dim, bias=False)
 
         self.leakyrelu = nn.LeakyReLU(alpha)
 
-    # def forward(self, h_n, adj_n):
-    #     with adj_n.local_scope():
-    #         adj_n.ndata['ft'] = h_n
-    #         # adj_n.apply_edges(fn.u_mul_v('ft','ft','e'))
-    #         adj_n.apply_edges(udf_u_muladd_v)
-    #         e = self.p(adj_n.edata['e'])
-    #         e= self.leakyrelu(e)
-    #         adj_n.edata['a'] = edge_softmax(adj_n, e)
-    #         adj_n.update_all(fn.u_mul_e('ft', 'a', 'm'), fn.sum('m', 'ft'))
-    #         rst = adj_n.ndata['ft']
-    #         return rst
-
-    # def forward(self, h_n, adj_n):
-    #     with adj_n.local_scope():
-    #         adj_n.nodes['item'].data['ft'] = h_n
-    #         adj_n.apply_edges(fn.u_mul_v('ft','ft','e'), etype='interacts')
-    #         e = self.p(adj_n.edges['interacts'].data['e'])
-    #         e= self.leakyrelu(e)
-    #         adj_n.edges['interacts'].data['a'] = edge_softmax(adj_n['interacts'], e)
-    #         adj_n.update_all(fn.u_mul_e('ft', 'a', 'm'), fn.sum('m', 'ft'), etype='interacts')
-    #         rst = adj_n.nodes['item'].data['ft']
-    #         return rst
-
-    def forward(self, h_n, h_p, h_t, adj_n):
-        with adj_n.local_scope():
+    def forward(self, h_n, h_p, h_t, g):
+        with g.local_scope():
 
             ###item to item
-            adj = adj_n.edge_type_subgraph(['interacts'])
+            adj = g.edge_type_subgraph(['interacts'])
             adj.ndata['ft'] = h_n
             adj.apply_edges(fn.u_mul_v('ft','ft','e'))
             e = self.p(adj.edata['e'])
@@ -120,15 +97,20 @@ class DglAggregator(nn.Module):
             
 
             ###agg
-            adj = adj_n.edge_type_subgraph(['agg'])
+            adj = g.edge_type_subgraph(['agg'])
             adj.nodes['target'].data['ft'] = h_t
             adj.edges['agg'].data['pos'] = h_p
             adj.apply_edges(fn.copy_src('ft','ft'))
             e = self.q(torch.cat([adj.edata['ft'], adj.edata['pos']], dim=-1))
             adj.edata['e'] = torch.tanh(e)
+
+            adj.update_all(fn.copy_edge('ft', 'm'), fn.mean('m', 'mean'))
+            f = self.r(torch.cat([adj.nodes['target'].data['ft'], adj.nodes['target'].data['mean']], dim=-1))
+            adj.nodes['target'].data['ft'] = f
+
             adj.update_all(udf_agg, fn.sum('m', 'ft'))
 
-            return adj_n.nodes['target'].data['ft']
+            return g.nodes['target'].data['ft']
 
 
 class SessionGraph(nn.Module):
@@ -161,38 +143,12 @@ class SessionGraph(nn.Module):
         for weight in self.parameters():
             weight.data.uniform_(-stdv, stdv)
         
-
-    # def forward(self, alias_inputs, mask, adj_n):
-    #     h_n = self.embedding(adj_n.nodes['item'].data['iid'])
-    #     h_local = self.local_agg(h_n, adj_n)
-    #     h_local = F.dropout(h_local, config.dropout_local, training=self.training)
-    #     hidden = h_local
-    #     get = lambda index: hidden[alias_inputs[index]]
-    #     hidden = torch.stack([get(i) for i in torch.arange(alias_inputs.shape[0]).long()])
-        
-    #     mask = mask.unsqueeze(-1)
-    #     batch_size = hidden.shape[0]
-    #     len = hidden.shape[1]
-    #     pos_emb = self.pos_embedding.weight[:len]
-    #     pos_emb = pos_emb.unsqueeze(0).repeat(batch_size, 1, 1)
-
-    #     hs = torch.sum(hidden * mask, -2) / torch.sum(mask, 1)
-    #     hs = hs.unsqueeze(-2).repeat(1, len, 1)
-    #     nh = torch.matmul(torch.cat([pos_emb, hidden], -1), self.w_1)
-    #     nh = torch.tanh(nh)
-    #     nh = torch.sigmoid(self.glu1(nh) + self.glu2(hs))
-    #     beta = torch.matmul(nh, self.w_2)
-    #     beta = beta * mask
-    #     select = torch.sum(beta * hidden, 1)
-    #     b = self.embedding.weight[1:config.num_node]  # n_nodes x latent_size
-    #     scores = torch.matmul(select, b.transpose(1, 0))
-    #     return scores
     
-    def forward(self, alias_inputs, mask, adj_n):
-        h_n = self.embedding(adj_n.nodes['item'].data['iid'])
-        h_p = self.pos_embedding(adj_n.edges['agg'].data['pid'])
-        h_r = self.target_embedding(adj_n.nodes['target'].data['tid'])
-        select = self.local_agg(h_n, h_p, h_r, adj_n)
+    def forward(self, g):
+        h_n = self.embedding(g.nodes['item'].data['iid'])
+        h_p = self.pos_embedding(g.edges['agg'].data['pid'])
+        h_r = self.target_embedding(g.nodes['target'].data['tid'])
+        select = self.local_agg(h_n, h_p, h_r, g)
         b = self.embedding.weight[1:config.num_node]  # n_nodes x latent_size
         scores = torch.matmul(select, b.transpose(1, 0))
         return scores
